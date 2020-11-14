0c3ae13bc170d0bc5a9e9894ec611b690ffa8ae0
Michael Stürmer <ms@mallorn.de>
2020-11-14 12:16:20 +0100

    initial import

---
+++ logger.go
@@ -0,0 +1,16 @@
+package log
+
+// Log interface
+type Logger interface {
+	// Print calls Output to print to the standard logger.
+	// Arguments are handled in the manner of fmt.Print.
+	Print(v ...interface{})
+
+	// Printf calls Output to print to the standard logger.
+	// Arguments are handled in the manner of fmt.Printf.
+	Printf(format string, v ...interface{})
+
+	// Println calls Output to print to the standard logger.
+	// Arguments are handled in the manner of fmt.Println.
+	Println(v ...interface{})
+}
